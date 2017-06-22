bcfde5b8dafcfb4ed3c9a550252914683e836805
John Son <jsson77@gmail.com>
2017-06-21 18:32:51 -0700

    replace card content with correct inputs

--- src/app/routes/Enigma/Enigma.js
+++ src/app/routes/Enigma/Enigma.js
@@ -1,31 +1,60 @@
 import React from 'react';
 import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card';
 import { Button } from 'react-toolbox/lib/button';
-import { Layout, NavDrawer, Panel, Sidebar } from 'react-toolbox';
+import Input from 'react-toolbox/lib/input';
 
-const dummyText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.';
 
-const TestCards = () => (
-  <Card style={{ width: '350px' }}>
-    <CardTitle
-      avatar="https://placeimg.com/80/80/animals"
-      title="Avatar style title"
-      subtitle="Subtitle here"
-    />
-    <CardMedia
-      aspectRatio="wide"
-      image="https://placeimg.com/800/450/nature"
-    />
-    <CardTitle
-      title="Title goes here"
-      subtitle="Subtitle here"
-    />
-    <CardText>{dummyText}</CardText>
-    <CardActions>
-      <Button label="Action 1" />
-      <Button label="Action 2" />
-    </CardActions>
-  </Card>
-);
+class Enigma extends React.Component {
+  constructor() {
+    super();
 
-export default TestCards;
+    this.state = {
+      name: '',
+      phone: '',
+      email: '',
+      hint: '',
+    };
+  }
+
+  handleChange(name, value) {
+    this.setState({ ...this.state, [name]: value });
+  }
+
+  //  TODO: Take first letter of name to populate icon
+  render() {
+    return (
+      <Card style={{ width: '350px'}}>
+        Tovia's Enigma
+        <Input
+          type="text"
+          label="Name"
+          required
+          onChange={this.handleChange.bind(this, 'hint')}
+          icon={<span>S</span>}
+        />
+        <Input
+          type="text"
+          multiline
+          label="Message *"
+          maxLength={120}
+          value={this.state.multiline}
+          onChange={this.handleChange.bind(this, 'multiline')}
+        />
+        <Input
+          type="date"
+          value={this.state.hint}
+          label="Expiration Date *"
+          hint="With Hint"
+          required
+          onChange={this.handleChange.bind(this, 'hint')}
+        />
+        <CardActions>
+          <Button label="ENCRYPT" />
+          <Button label="DECRYPT" />
+        </CardActions>
+      </Card>
+    );
+  }
+}
+
+export default Enigma;
